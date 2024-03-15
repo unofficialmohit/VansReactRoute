c8df5847611ddade498af43edb4028c09701c5ba
mohit <mohitgujjar2121@gmail.com>
2024-03-15 17:20:35 +0530

    404notFoundPageAdded

--- index.jsx
+++ index.jsx
@@ -15,6 +15,7 @@ import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
+import NotFound from "./components/NotFound"
 
 import "./server"
 
@@ -39,6 +40,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
