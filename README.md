f9e73f1456c519ec94159b53342c5a403a830c13
mohit <mohitgujjar2121@gmail.com>
2024-03-22 16:32:23 +0530

    Action and Protected Routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# co78c4675a6da142e82343e55
+# co5cc482e8e3d88fc08865c34
 
 Warning: Vite enforces using jsx syntax inside jsx/tsx files, so it will complain about that. Solution: rename `.js` files to `.jsx` :)
 
--- components/Error.jsx
+++ components/Error.jsx
@@ -1,7 +1,13 @@
 import React from "react"
+import { useRouteError } from "react-router-dom"
 
 export default function Error() {
+    const error = useRouteError()
+    
     return (
-        <h1>An error occured!</h1>
+        <>
+        <h1>Error: {error.message}</h1>
+        <pre>{error.status} - {error.statusText}</pre>
+        </>
     )
 }
